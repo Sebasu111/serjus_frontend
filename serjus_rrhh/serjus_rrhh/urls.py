e0efff1895982e906703c7cef9e76d90a912a1a5
JoseSerra2 <123127607+JoseSerra2@users.noreply.github.com>
2025-09-24 19:00:53 -0600

    minicambios

--- serjus_rrhh/serjus_rrhh/urls.py
+++ serjus_rrhh/serjus_rrhh/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("api/", include("rrhh.urls")),
 
     # Schema y documentación
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
