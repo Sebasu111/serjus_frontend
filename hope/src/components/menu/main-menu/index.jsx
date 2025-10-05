f66cbb50ae33c1e5582f7e2b2feb6ccbddbbd1cf
melany0330 <melbelen30@gmail.com>
2025-10-04 18:01:45 -0600

    reordenar elementos del menú en el sidebar
    Se ajustó el orden de los elementos en el sidebar

--- hope/src/components/menu/main-menu/index.jsx
+++ hope/src/components/menu/main-menu/index.jsx
@@ -1,160 +1,145 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
+import { 
+  FaHome, FaUsers, FaUserTie, FaClipboardList, FaBook, FaFileAlt, FaSignOutAlt
+} from "react-icons/fa";
 import "./sidebar-menu.css";
 
 const base = process.env.PUBLIC_URL || "";
 
 const SidebarMenu = () => {
-    // Estado para controlar submenús abiertos
-    const [openMenu, setOpenMenu] = useState(null);
+  const [openMenu, setOpenMenu] = useState(null);
+  const history = useHistory(); // <-- useHistory en v5
 
-    const toggleSubmenu = (menuName) => {
-        setOpenMenu(openMenu === menuName ? null : menuName);
-    };
+  const toggleSubmenu = (menuName) => {
+    setOpenMenu(openMenu === menuName ? null : menuName);
+  };
 
-    return (
-        <nav className="sidebar-menu">
-            <ul className="sidebar-menu-list">
-                {/* Botón Home */}
-                <li>
-                    <NavLink
-                        to={`${base}/home`}
-                        className="sidebar-menu-link home-link"
-                        activeClassName="active"
-                        style={{
-                            fontWeight: "700",
-                            color: "#ffffffff",
-                            marginBottom: "15px",
-                            display: "block",
-                        }}
-                    >
-                        Inicio
-                    </NavLink>
-                </li>
+  const handleLogout = () => {
+    localStorage.removeItem("usuarioLogueado");
+    sessionStorage.removeItem("idUsuario");
+    history.push("/login"); // <-- redirige al login en v5
+  };
 
-                {/* Gestión de Usuarios y Seguridad */}
-                <li>
-                    <div
-                        className="sidebar-menu-link"
-                        onClick={() => toggleSubmenu("usuarios")}
-                    >
-                        Gestión de Usuarios
-                    </div>
-                    {openMenu === "usuarios" && (
-                        <ul className="sidebar-submenu">
-                            <li>
-                                <NavLink to={`${base}/Usuarios`} className="sidebar-submenu-link">
-                                    Usuarios
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to={`${base}/Roles`} className="sidebar-submenu-link">
-                                    Roles y Permisos
-                                </NavLink>
-                            </li>
-                        </ul>
-                    )}
-                </li>
+  return (
+    <nav className="sidebar-menu">
+      <div className="sidebar-logo">
+        <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="Logo" />
+      </div>
 
-                {/* Gestión de Catálogos */}
-                <li>
-                    <div
-                        className="sidebar-menu-link"
-                        onClick={() => toggleSubmenu("catalogos")}
-                    >
-                        Gestión de Catálogos
-                    </div>
-                    {openMenu === "catalogos" && (
-                        <ul className="sidebar-submenu">
-                            <li><NavLink to={`${base}/Idiomas`} className="sidebar-submenu-link">Idiomas</NavLink></li>
-                            <li><NavLink to={`${base}/Estados`} className="sidebar-submenu-link">Estados</NavLink></li>
-                            <li><NavLink to={`${base}/Puesto`} className="sidebar-submenu-link">Puestos</NavLink></li>
-                            <li><NavLink to={`${base}/TiposDocumento`} className="sidebar-submenu-link">Tipos de Documentos</NavLink></li>
-                            <li><NavLink to={`${base}/PuebloCultura`} className="sidebar-submenu-link">Pueblos y Culturas</NavLink></li>
-                        </ul>
-                    )}
-                </li>
+      <ul className="sidebar-menu-list">
+        {/* Inicio */}
+        <li>
+          <NavLink to={`${base}/home`} className="sidebar-menu-link">
+            <FaHome className="menu-icon" /> Inicio
+          </NavLink>
+        </li>
 
-                {/* Gestión de Empleados */}
-                <li>
-                    <div
-                        className="sidebar-menu-link"
-                        onClick={() => toggleSubmenu("empleados")}
-                    >
-                        Gestión de Empleados
-                    </div>
-                    {openMenu === "empleados" && (
-                        <ul className="sidebar-submenu">
-                            <li><NavLink to={`${base}/Empleados`} className="sidebar-submenu-link">Empleados</NavLink></li>
-                            <li><NavLink to={`${base}/HistorialPuesto`} className="sidebar-submenu-link">Historial de Puestos</NavLink></li>
-                            <li><NavLink to={`${base}/Contratos`} className="sidebar-submenu-link">Contratos</NavLink></li>
-                            <li><NavLink to={`${base}/Documentos`} className="sidebar-submenu-link">Documentos</NavLink></li>
-                        </ul>
-                    )}
-                </li>
-
-                {/* Recursos Humanos */}
-                <li>
-                    <div
-                        className="sidebar-menu-link"
-                        onClick={() => toggleSubmenu("rrhh")}
-                    >
-                        Recursos Humanos
-                    </div>
-                    {openMenu === "rrhh" && (
-                        <ul className="sidebar-submenu">
-                            <li><NavLink to={`${base}/Amonestacion`} className="sidebar-submenu-link">Amonestaciones</NavLink></li>
-                            <li><NavLink to={`${base}/Ausencia`} className="sidebar-submenu-link">Ausencias</NavLink></li>
-                            <li><NavLink to={`${base}/TerminacionLaboral`} className="sidebar-submenu-link">Terminación Laboral</NavLink></li>
-                        </ul>
-                    )}
-                </li>
-
-                {/* Selección y Capacitación */}
-                <li>
-                    <div
-                        className="sidebar-menu-link"
-                        onClick={() => toggleSubmenu("seleccion")}
-                    >
-                        Selección y Capacitación
-                    </div>
-                    {openMenu === "seleccion" && (
-                        <ul className="sidebar-submenu">
-                            <li><NavLink to={`${base}/Aspirantes`} className="sidebar-submenu-link">Aspirantes</NavLink></li>
-                            <li><NavLink to={`${base}/Convocatorias`} className="sidebar-submenu-link">Convocatorias</NavLink></li>
-                            <li><NavLink to={`${base}/Evaluaciones`} className="sidebar-submenu-link">Evaluaciones</NavLink></li>
-                            <li><NavLink to={`${base}/CriterioEvaluacion`} className="sidebar-submenu-link">Criterios de Evaluación</NavLink></li>
-                            <li><NavLink to={`${base}/EvaluacionCriterio`} className="sidebar-submenu-link">Evaluación por Criterios</NavLink></li>
-                            <li><NavLink to={`${base}/EmpleadoCapacitacion`} className="sidebar-submenu-link">Empleado - Capacitación</NavLink></li>
-                            <li><NavLink to={`${base}/Inducciones`} className="sidebar-submenu-link">Inducciones</NavLink></li>
-                            <li><NavLink to={`${base}/InduccionDocumentos`} className="sidebar-submenu-link">Documentos de Inducción</NavLink></li>
-                        </ul>
-                    )}
-                </li>
-
-                {/* Equipos */}
-                <li>
-                    <NavLink to={`${base}/Equipos`} className="sidebar-menu-link">Equipos</NavLink>
-                </li>
-                {/* Cerrar Sesion */}
-                <li>
-                    <NavLink
-                        to={`${base}/`}
-                        className="sidebar-menu-link home-link"
-                        activeClassName="active"
-                        style={{
-                            fontWeight: "700",
-                            color: "#ffffffff",
-                            marginBottom: "15px",
-                            display: "block",
-                        }}
-                    >
-                        Cerrar Sesion
-                    </NavLink>
-                </li>
+        {/* Personal */}
+        <li>
+          <div className="sidebar-menu-link" onClick={() => toggleSubmenu("personal")}>
+            <FaUsers className="menu-icon" /> Personal
+          </div>
+          {openMenu === "personal" && (
+            <ul className="sidebar-submenu">
+              <li><NavLink to={`${base}/Empleados`} className="sidebar-submenu-link">Empleados</NavLink></li>
+              <li><NavLink to={`${base}/Contrato`} className="sidebar-submenu-link">Contrato</NavLink></li>
+              <li><NavLink to={`${base}/Historial`} className="sidebar-submenu-link">Historial</NavLink></li>
+              <li><NavLink to={`${base}/Usuarios`} className="sidebar-submenu-link">Usuarios</NavLink></li>
             </ul>
-        </nav>
-    );
+          )}
+        </li>
+
+        {/* Reclutamiento */}
+        <li>
+          <div className="sidebar-menu-link" onClick={() => toggleSubmenu("reclutamiento")}>
+            <FaClipboardList className="menu-icon" /> Reclutamiento
+          </div>
+          {openMenu === "reclutamiento" && (
+            <ul className="sidebar-submenu">
+              <li><NavLink to={`${base}/Convocatorias`} className="sidebar-submenu-link">Convocatorias</NavLink></li>
+              <li><NavLink to={`${base}/Aspirantes`} className="sidebar-submenu-link">Aspirantes</NavLink></li>
+              <li><NavLink to={`${base}/Seleccion`} className="sidebar-submenu-link">Selección / Contratación</NavLink></li>
+            </ul>
+          )}
+        </li>
+
+        {/* Integración */}
+        <li>
+          <div className="sidebar-menu-link" onClick={() => toggleSubmenu("integracion")}>
+            <FaBook className="menu-icon" /> Integración
+          </div>
+          {openMenu === "integracion" && (
+            <ul className="sidebar-submenu">
+              <li><NavLink to={`${base}/Induccion`} className="sidebar-submenu-link">Inducción</NavLink></li>
+              <li><NavLink to={`${base}/Capacitacion`} className="sidebar-submenu-link">Capacitación</NavLink></li>
+            </ul>
+          )}
+        </li>
+
+        {/* Desempeño */}
+        <li>
+          <div className="sidebar-menu-link" onClick={() => toggleSubmenu("desempeno")}>
+            <FaClipboardList className="menu-icon" /> Desempeño
+          </div>
+          {openMenu === "desempeno" && (
+            <ul className="sidebar-submenu">
+              <li><NavLink to={`${base}/Evaluaciones`} className="sidebar-submenu-link">Evaluaciones</NavLink></li>
+              <li><NavLink to={`${base}/Criterios`} className="sidebar-submenu-link">Criterios</NavLink></li>
+            </ul>
+          )}
+        </li>
+
+        {/* Control Disciplinario */}
+        <li>
+          <div className="sidebar-menu-link" onClick={() => toggleSubmenu("disciplinario")}>
+            <FaUserTie className="menu-icon" /> Control Disciplinario
+          </div>
+          {openMenu === "disciplinario" && (
+            <ul className="sidebar-submenu">
+              <li><NavLink to={`${base}/Ausencias`} className="sidebar-submenu-link">Ausencias</NavLink></li>
+              <li><NavLink to={`${base}/Amonestaciones`} className="sidebar-submenu-link">Amonestaciones</NavLink></li>
+            </ul>
+          )}
+        </li>
+
+        {/* Catálogo */}
+        <li>
+          <div className="sidebar-menu-link" onClick={() => toggleSubmenu("catalogo")}>
+            <FaBook className="menu-icon" /> Catálogo
+          </div>
+          {openMenu === "catalogo" && (
+            <ul className="sidebar-submenu">
+              <li><NavLink to={`${base}/CulturaPueblo`} className="sidebar-submenu-link">Cultura Pueblo</NavLink></li>
+              <li><NavLink to={`${base}/Idioma`} className="sidebar-submenu-link">Idioma</NavLink></li>
+              <li><NavLink to={`${base}/Puestos`} className="sidebar-submenu-link">Puestos</NavLink></li>
+            </ul>
+          )}
+        </li>
+
+        {/* Informes */}
+        <li>
+          <NavLink to={`${base}/Informes`} className="sidebar-menu-link">
+            <FaFileAlt className="menu-icon" /> Informes
+          </NavLink>
+        </li>
+
+        {/* Documentos */}
+        <li>
+          <NavLink to={`${base}/Documentos`} className="sidebar-menu-link">
+            <FaFileAlt className="menu-icon" /> Documentos
+          </NavLink>
+        </li>
+
+        {/* Cerrar Sesión */}
+        <li>
+          <NavLink to={`${base}/`} className="sidebar-menu-link logout-link">
+            <FaSignOutAlt className="menu-icon" /> Cerrar Sesión
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default SidebarMenu;
